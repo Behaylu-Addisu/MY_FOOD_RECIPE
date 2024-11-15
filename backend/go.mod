f8a48cd25339cc2dd3b20bf8eb3a406975aa3364
behayluN <addisubehaylu@gmail.com>
2024-11-15 23:24:07 +0300

    update4

--- backend/go.mod
+++ backend/go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
 )
--- backend/models/ingredients.go
+++ backend/models/ingredients.go
@@ -1,17 +1,17 @@
 package models
 
 import (
-    "github.com/google/uuid"
+    "github.com/google/uuid" // Correct import for the uuid package
     "time"
 )
 
 // Ingredient represents an ingredient for a recipe.
 type Ingredient struct {
-    ID            uuid.UUID `json:"id" db:"id"` // UUID as primary key
-    RecipeID      uuid.UUID `json:"recipe_id" db:"recipe_id"` // Foreign key to recipes table
-    IngredientName string   `json:"ingredient_name" db:"ingredient_name"`
-    Quantity      string    `json:"quantity" db:"quantity"`
-    Unit          string    `json:"unit" db:"unit"`
-    CreatedAt     time.Time `json:"created_at" db:"created_at"`
-    UpdatedAt     time.Time `json:"updated_at" db:"updated_at"`
+    ID             uuid.UUID `json:"id" db:"id"`               // UUID as primary key
+    RecipeID       uuid.UUID `json:"recipe_id" db:"recipe_id"` // Foreign key to recipes table
+    IngredientName string    `json:"ingredient_name" db:"ingredient_name"`
+    Quantity       string    `json:"quantity" db:"quantity"`
+    Unit           string    `json:"unit" db:"unit"`
+    CreatedAt      time.Time `json:"created_at" db:"created_at"`
+    UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
 }
