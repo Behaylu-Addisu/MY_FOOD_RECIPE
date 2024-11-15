19f15aeb02d9b57e275a57bbbca084cc7e6e58c9
behayluN <addisubehaylu@gmail.com>
2024-11-15 21:34:18 +0300

    empty commit

--- backend/go.mod
+++ backend/go.mod
@@ -1,12 +1,11 @@
-module backend
+module github.com/Behaylu-Addisu/MY_FOOD_RECIPE/backend
 
 go 1.19
 
 require (
-    github.com/dgrijalva/jwt-go v3.2.0+incompatible
-    github.com/gorilla/mux v1.8.0
-    github.com/joho/godotenv v1.4.0
-    github.com/jmoiron/sqlx v1.3.5
-    github.com/lib/pq v1.10.0
-    golang.org/x/crypto/bcrypt v0.0.0-20210921015540-d9e4728a5b75
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.8.1
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.0
 )
--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"net/http"
 	"github.com/Behaylu-Addisu/MY_FOOD_RECIPE/backend/handlers"
 )
 
@@ -16,14 +16,19 @@ var db *sqlx.DB
 var err error
 
 func main() {
-	// Load environment variables
+	// Load environment variables from .env file
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// Connect to PostgreSQL database
-	dbURL := os.Getenv("postgres://behaylu:ngst@postgres:5432/food_recipe_db")
+	// Get the database URL from the environment variable
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable not set")
+	}
+
+	// Connect to the PostgreSQL database
 	db, err = sqlx.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
@@ -45,7 +50,8 @@ func main() {
 	// Start the server
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8080" // Default port
 	}
+	log.Printf("Server starting on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
